3cdde136b1647ab57805f36473af0956251d0897
Mauricio Magalhaes <contato@mauriciomagalhaes.com.br>
2021-07-20 13:56:38 -0300

    remove old

--- backsync.sh
+++ backsync.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
-
+#
 # yum install rclone
 # rclone config
-
+#
 RCLONE=$(which rclone)
-SRCDIR="/var/www/backup"
+DEL=$(which find)
+#
+SRCDIR="/var/www/backup/"
 DSTDIR="/Backups/Asterisk/nome_remoto/"
 REMOTE="nome_remoto"
-# suffix ms|s|m|h|d|w|M|y
-QTDDIAS="3d"
-
-$RCLONE --log-file=rclone.log -v -P sync --max-age $QTDDIAS  $SRCDIR --exclude="*.php" $REMOTE:$DSTDIR
+QTDIAS="+3"
+#
+$DEL $SRCDIR -type f -name '*.tar' -mtime $QTDIAS -exec rm -f {} \;
+$RCLONE --log-file=rclone.log -v -P sync $SRCDIR --exclude="*.php" $REMOTE:$DSTDIR
