6b52b47f1244c448983f9564bcbe8215db0261ec
Mauricio Magalhaes <contato@mauriciomagalhaes.com.br>
2021-07-20 11:34:15 -0300

    .

--- backsync.sh
+++ backsync.sh
@@ -8,5 +8,4 @@ SRCDIR="/var/www/backup"
 DSTDIR="/Backups/Asterisk/nome_remoto/"
 REMOTE="nome_remoto"
 
-
 $RCLONE --log-file=rclone.log -v sync --max-age 3d  $SRCDIR --exclude="*.php" $REMOTE:$DSTDIR
