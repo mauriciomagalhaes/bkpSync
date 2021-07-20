74e0e2c325b033238ff704e7c2277f693a581627
Mauricio Magalhaes <contato@mauriciomagalhaes.com.br>
2021-07-20 14:46:56 -0300

    .

--- backsync.sh
+++ backsync.sh
@@ -12,6 +12,6 @@ DSTDIR="/Backups/Asterisk/nome_remoto/"
 REMOTE="nome_remoto"
 QTDIAS="+3"
 #
-$PHP /var/www/backup/automatic_backup.php
+$PHP $SRCDIR/automatic_backup.php
 $DEL $SRCDIR -type f -name '*.tar' -mtime $QTDIAS -exec rm -f {} \;
 $RCLONE --log-file=rclone.log -v -P sync $SRCDIR --exclude="*.php" $REMOTE:$DSTDIR
