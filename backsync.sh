715ccd2c36883e64098227f2d528b1ad95293e4f
Maurício Magalhães <contato@mauriciomagalhaes.com.br>
2023-09-01 21:35:39 -0300

    Update backsync.sh
    variavel central hostname

--- backsync.sh
+++ backsync.sh
@@ -6,12 +6,13 @@
 RCLONE=$(which rclone)
 DEL=$(which find)
 PHP=$(which php)
+IPBXHOSTNAME=$(hostname | cut -d. -f1)
 #
 SRCDIR="/var/www/backup/"
-DSTDIR="/Backups/Asterisk/nome_remoto/"
-REMOTE="nome_remoto"
+DSTDIR="/T3telecom/BACKUP/ISSABEL"
+REMOTE="Backup"
 QTDIAS="+3"
 #
-$PHP $SRCDIR/automatic_backup.php
+#$PHP $SRCDIR/automatic_backup.php
 $DEL $SRCDIR -type f -name '*.tar' -mtime $QTDIAS -exec rm -f {} \;
-$RCLONE --log-file=rclone.log -v -P sync $SRCDIR --exclude="*.php" $REMOTE:$DSTDIR
+$RCLONE --log-file=rclone.log -v -P sync $SRCDIR --exclude="*.php" $REMOTE:$DSTDIR/${IPBXHOSTNAME^^}
